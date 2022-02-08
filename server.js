ca02ae48df1d7e94721757b5b50c1da114066392
mona <csimonusa@outlook.com>
2022-02-08 16:23:52 -0500

    updated server.js

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ const PORT = process.env.PORT || 3001;
 
 
 // Need a functionality do we need here?
+const sequelize = require('./config/config')
+const sequelizeStore = require('connect-session-sequelize')(session.Store)
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize
+  })
+}
 
-
+app.use(session(sess))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
 module.exports = {
   format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/$`
     // How do we format the date and time?
   }
 };
