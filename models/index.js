af0c7920ba2fbd2f529d1ee5960a78593745359d
mona <csimonusa@outlook.com>
2022-02-08 16:16:01 -0500

    added auth helper

--- models/index.js
+++ models/index.js
@@ -1,5 +1,22 @@
-
+const Comment = require('./Comment')
+const Post = require('./Post')
+const User = require('./User')
 //What do we need to require for this document?
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+})
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+})
 
 module.exports = {
   User,
--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,9 @@
 const withAuth = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/login')
+  } else {
+    next()
+  }
   // What functionality are we missing?
 
 };
