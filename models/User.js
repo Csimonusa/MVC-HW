d32f2ef4d640fc604ad316cd64620a44487ffb1d
mona <csimonusa@outlook.com>
2022-02-08 16:09:53 -0500

    refactored activity 17 hooks and added model properties

--- models/User.js
+++ models/User.js
@@ -1,16 +1,38 @@
 // What do we need to require here?
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/config')
+const bcrypt = require('bcrypt')
 
 // create our User model
 class User extends Model {
+  checkPass(password) {
+    return bcrypt.compareSync(password, this.password)
+  }
   // set up method to run on instance data (per user) to check password
 }
 
 User.init(
   {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   //  What needs to go in the User expression here?
   },
   {
     hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10)
+        return newUserData
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+        return updatedUserData
+      }
       // How do we set up the hook functionality?
     
     },
