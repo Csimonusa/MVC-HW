8534f6fe8a42472d16c005f1678e011224abb537
mona <csimonusa@outlook.com>
2022-02-08 16:00:58 -0500

    added model for post

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,17 @@
 // What are we missing to be required here?
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/config')
 
 class Post extends Model {}
 
 Post.init(
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  },
+  {
+    sequelize
+  }
   // Finish the post expression
 );
 
