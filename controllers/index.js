401bf7718955e577fcbae57a08dd99efcdb23256
mona <csimonusa@outlook.com>
2022-02-21 13:16:52 -0500

    required and used routes in index cont

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
 //Create the api routes and require them to use them within the controllers
+const apiRoutes = require('./api')
+const homeRoutes = require('./home-routes')
+const dashboardRoutes = require('./dashboard-routes')
+
+router.use('/api', apiRoutes)
+router.use('/', homeRoutes)
+router.use('/dashboard', dashboardRoutes)
 
 module.exports = router;
