3dd0a7bedb71c5fe6f4660002025f2c98db5431a
mona <csimonusa@outlook.com>
2022-02-21 13:21:00 -0500

    home route that gets all post and renders to the all-post view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,16 @@ const { Post, Comment, User } = require('../models');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  
+  try {
+      const postData = await Post.findAll({ include: [User]})
+      
+      const posts = postData.map((post) => post.get({plain: true}))
+
+      res.render('all-post', {posts})
+
+  } catch (error) {
+      res.json(err)
+  }
 });
 
 // get single post
