531c85bb39f2041467bd758aa2fa4d82a1fa7ccc
mona <csimonusa@outlook.com>
2022-02-21 13:34:30 -0500

    new and home post routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth')
+const {Post} = require('../models/')
 //Require the correct files from the models and authorizations
 
 router.get('/', withAuth, async (req, res) => {
   //Create the correct asychronous get route for this function
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId
+      }
+    })
+    const posts = postData.map((post) => post.get({plain:true}))
+    res.render('all-post-admin', {
+      layout: 'dashboard',
+      posts
+    })
+  } catch (error) {
+    res.redirect('login')
+  }
 });
 
 router.get('/new', withAuth, (req, res) => {
