29cd6fb5ca8a503bd41b52eae8380e3b28e18ed4
mona <csimonusa@outlook.com>
2022-02-21 13:39:29 -0500

    comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const {Comment} = require('../../models')
+const withAuth = require('../../utils/auth')
 
 //Require the correct files from the models and authorizations
 
 router.post('/', withAuth, async (req, res) => {
   // Complete the asynchronous function for router.post
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      userId: req.session.userId
+    })
+    res.json(newComment)
+  } catch (error) {
+    res.json(error)
+  }
 });
 
 module.exports = router;
